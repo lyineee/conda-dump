ce5e4144ac4a9bb71ab8e65ba7e8c1f07f87324e
lyine <liuzhengyuan@hotmail.com>
2022-10-20 13:20:59 +0800

    feat: command line options

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import json
-import os
+import os, sys
 import shutil
 from packaging import version
-from typing import Optional
+from typing import Optional, List
 
 from dep_tree import DependInfo, DependReq, DependTree
 
-conda_base = "C:/Users/liuzh/miniconda3/envs"
+conda_base = "~/Miniconda3"
+env_name = ""
+print_tree = False
+dry_run = False
+dump_path = "~/Document/dump"
 
 
 def dep_info_parser(data: dict) -> DependInfo:
@@ -15,15 +19,15 @@ def dep_info_parser(data: dict) -> DependInfo:
     )
     if data["link"]["source"] != "":
         info.link = data["link"]["source"].replace("\\", "/")
-
+        
     # parse depend requirements
     for req_str in data["depends"]:
         info.depends.append(dep_req_parser(req_str))
-
+        
     return info
 
 
-def dep_req_parser(dep_str: str) -> DependReq:
+def dep_req_parser(dep_str:str) -> DependReq:
     dep_req = DependReq(dep_str.split()[0])
     if len(dep_str.split()) > 1:
         for req in dep_str.split()[1].split(","):
@@ -42,8 +46,31 @@ def dep_req_parser(dep_str: str) -> DependReq:
     return dep_req
 
 
+def search_dep_iter(dep_req: DependReq) -> DependInfo:
+    base = conda_base + env_name
+    prefix = "/conda-meta"
+    for dep in dep_list:
+        found_flag = False
+        for path in os.listdir(base + prefix):
+            info = os.path.split(path)[-1].split("-")
+            if len(info) < 2:
+                continue
+            info = ["-".join(info[:-2]), info[-2]]
+            if info[0] == dep:
+                if not dep.meet(version.Version(info[1])):
+                    print(
+                        f"found depend {info}, but do not match req {dep}, continue searching"
+                    )
+            if info[0] == dep["name"] and test_version(info[1], dep):
+                found_flag = True
+                yield "conda-meta/" + path
+                break
+
+        if not found_flag:
+            print(f'WARN: deppend {dep["name"]} no found')
+
 def search_dep(dep_req: DependReq) -> DependInfo:
-    base = conda_base + "/envs/notebook"
+    base = conda_base  + env_name
     prefix = "/conda-meta"
     for path in os.listdir(base + prefix):
         info = os.path.split(path)[-1].split("-")
@@ -56,33 +83,43 @@ def search_dep(dep_req: DependReq) -> DependInfo:
                     f"found depend {info}, but do not match req {dep}, continue searching"
                 )
             else:
-                with open(base + "/conda-meta/" + path, "r") as f:
+                with open(base + "/conda-meta/" + path, 'r') as f:
                     data = json.load(f)
                     return dep_info_parser(data)
-    raise RuntimeWarning(f'deppend {dep} no found')
+                    
+    print(f'WARN: deppend {dep_req} no found')
 
 
-def dep(dep_info: DependInfo) -> DependTree:
+def dep(dep_info:DependInfo, ex_list:List[DependReq] = []) -> DependTree:
+    conda_path = conda_base + env_name
     node = DependTree(dep_info)
     if len(dep_info.depends) == 0:
         return node
     for dep_req in dep_info.depends:
+        for exclude in ex_list:
+            ex_flag = False
+            if exclude.name == dep_req.name:
+                ex_flag = True
+        if ex_flag:
+            continue
         info = search_dep(dep_req)
         if info.name in ["vs2015_runtime", "vc", "python"] or info in node:
             continue
-        node.append(dep(info))
+
+        node.append(dep(info, ex_list))
     return node
 
 
-def get_lib(dep_tree: DependTree) -> set:
+def get_lib(dep_tree: DependTree)->set:
     file_set = set()
     for dep_info in dep_tree:
         file_set.update(dep_info.files)
     return file_set
 
 
-def copy_all(lib_list, base="./Documents/note-book"):
-    conda_path = conda_base + "/envs/notebook"
+def copy_all(lib_list, base):
+    base = os.path.expanduser(base)
+    conda_path = conda_base + env_name
     err_count = 0
     copy_count = 0
     all_count = len(lib_list)
@@ -101,23 +138,73 @@ def copy_all(lib_list, base="./Documents/note-book"):
         if not os.path.exists(dst):
             shutil.copy(src, dst)
             copy_count += 1
-
+            
         # progress bar
         file_name = os.path.split(file_path)[-1]
         width = 20
         progress_count += 1
-        c = int((progress_count / all_count) * width) + 1
-        bar = "|" + (c * "-").ljust(width) + "|"
-        print(f'{bar}{progress_count}/{all_count} processing "{file_name}"', end="")
-        print(f'{" "*100}', end="\r")  # clean line
+        c = int((progress_count/all_count)*width) + 1
+        bar = '|'+(c*"-").ljust(width)  + '|'
+        print(f'{bar}{progress_count}/{all_count} processing "{file_name}"', end='')
+        print(f'{" "*100}', end='\r') # clean line
         # print(f'{copy_count}/{all_count}  copying "{src}" to "{dst}"', end="\r")
-    print("")
-    print(
-        f"finish copy, total: {len(lib_list)}, copy: {copy_count}, error: {err_count}"
-    )
+    print('')
+    print(f'finish copy, total: {len(lib_list)}, copy: {copy_count}, error: {err_count}')
 
+def dump(package:str, exclude_list:List[str]):
+    conda_env_name = (lambda: "base" if env_name == "" else os.path.split(env_name)[-1])()
+    print(f'dump package "{package}" in envirment "{conda_env_name}", exclude_list: {exclude_list}')
+    print(f'dump destination path: "{dump_path}"')
+    req = DependReq(name=package)
+    dep_info = search_dep(req)
+    if not dep_info:
+        print(f'ERROR: package "{package}" not found in conda envirment "{conda_env_name}", dump stop')
+        sys.exit(1)
+    ex = []
+    for exclude in exclude_list:
+        ex.append(DependReq(name=exclude))
+    print(f'parsing dependent tree')
+    dep_tree = dep(dep_info, ex)
+    if print_tree:
+        dep_tree.print_struct()
+    if not dry_run:
+        print(f'start copying file to {dump_path}')
+        copy_all(get_lib(dep_tree), dump_path)
+            
+def init_args():
+    import argparse
+    func_list = ['dump', 'restore']
+    parser = argparse.ArgumentParser(prog="conda-dump")
+    parser.add_argument('subcommand',type=str, choices=func_list)
+    parser.add_argument('package',type=str, help="specify a package to dump")
+    parser.add_argument('-n','--conda-env', type=str, dest="conda_env", help="specify a envirment name")
+    parser.add_argument('-c', '--conda-path', type=str,dest="conda_path", help="specify a conda base path")
+    parser.add_argument('-e', '--exclude', action="append",type=str, help="exclude exist package")
+    parser.add_argument('-d','--dump-path',dest="dump_path", help="dump destination directory")
+    parser.add_argument('-t', '--print-tree',action="store_true",dest="print_tree", help="print dependent tree")
+    parser.add_argument('--dry-run',action="store_true",dest="dry_run", help="dry run")
+    args = parser.parse_args()
+    global conda_base, env_name, print_tree, dry_run, dump_path
+    if args.conda_path:
+        conda_base = args.conda_path
+    conda_base = os.path.expanduser(conda_base).replace('\\','/')
+    if not os.path.exists(conda_base):
+        print(f'conda path not found in default path {conda_base}, please specify another')
+        sys.exit(1)
+    if args.conda_env:
+        env_name = "/envs/" + args.conda_env
+    print_tree = args.print_tree
+    dry_run = args.dry_run
+    if args.dump_path:
+        dump_path = args.dump_path
+    dump_path = os.path.expanduser(dump_path).replace('\\','/')
+        
+    print(f'working on conda path: "{conda_base}"')
+    if args.subcommand == 'dump':
+        dump(args.package, args.exclude)
+    elif args.subcommand == 'restore':
+        print('not implement yet')
+    
 
 if __name__ == "__main__":
-    req = DependReq(name="notebook")
-    dep_info = search_dep(req)
-    a = dep(dep_info)
+    init_args()
